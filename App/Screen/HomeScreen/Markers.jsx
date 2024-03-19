5fcdd332d19e62f88cca7238ce2649e2c6bb187c
Nathaniel <nathanielyew@gmail.com>
2024-03-19 15:58:35 +0800

    add marker png

--- App/Screen/HomeScreen/Markers.jsx
+++ App/Screen/HomeScreen/Markers.jsx
@@ -5,7 +5,6 @@ import { SelectMarkerContext } from '../../Context/SelectMarkerContext'
 
 export default function Markers({ index, place }) {
 
-    // const { selectedMarker, setSelectedMarker } = useContext(SelectMarkerContext);
     return place && (
 
         <Marker
@@ -13,19 +12,7 @@ export default function Markers({ index, place }) {
                 latitude: place.location?.latitude,
                 longitude: place.location?.longitude
             }}
-
-        // onPress={() => setSelectedMarker(index)}
         >
-
-            {/* {selectedMarker == index ?
-                <Image source={require('./../../../assets/images/marker-selected.png')}
-                    style={{ width: 60, height: 60 }}
-                />
-                : <Image source={require('./../../../assets/images/marker.png')}
-                    style={{ width: 60, height: 60 }}
-                />} */}
-
-
         </Marker>
 
 
