607d7f0da7953e93847bbde090f2eb8ea373b572
Samantha Tan <samanthatan@Tans-MacBook-Air.local>
2024-03-24 21:33:02 +0800

    revert changes

--- App/Screen/HomeScreen/AppMapView.jsx
+++ App/Screen/HomeScreen/AppMapView.jsx
@@ -1,24 +1,22 @@
-import { View, Text } from 'react-native'
-import React, { useContext } from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React, { useContext, useEffect } from 'react'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
-import { StyleSheet } from 'react-native'
 import MapViewStyle from '../../Utils/MapViewStyle.json'
 import { UserLocationContext } from '../../Context/UserLocationContext'
-import { Image } from 'react-native'
 import Markers from './Markers'
 
 export default function AppMapView({placeList}) {
     const { location, setLocation } = useContext(UserLocationContext);
-    return location?.latitude&&(
+    return location?.latitude && (
         <View>
             <MapView style={styles.map}
                 provider={PROVIDER_GOOGLE}
                 customMapStyle={MapViewStyle}
                 region={{
-                    latitude:location?.latitude,
-                    longitude:location?.longitude,
-                    latitudeDelta:0.0422,
-                    longitudeDelta:0.0421
+                    latitude: location?.latitude,
+                    longitude: location?.longitude,
+                    latitudeDelta: 0.0422,
+                    longitudeDelta: 0.0421
                 }}>
                     {location?<Marker
                         coordinate={{
--- App/Utils/GlobalApi.js
+++ App/Utils/GlobalApi.js
@@ -8,6 +8,7 @@ const config= {
     headers:{
         'Content-Type':'application/json',
         'X-Goog-Api-Key': "AIzaSyDCcI9PY0s1yv_KsRJxFlKqLuZIwVjD14A",
+
         //get the approriate info from the API 
         'X-Goog-FieldMask' : [
             'places.displayName',
@@ -22,10 +23,21 @@ const config= {
 }
 
 //Make the API calls
-const NewNearByPlace=(data)=>axios.post(BASE_URL, data, config);
+const NewNearByPlace = (data) => axios.post(BASE_URL, data, config);
+
+// Function to make Distance Matrix API call
+const calculateDistance = (origins, destinations) => axios.get(BASE_URL_DISTANCE_MATRIX, {
+    ...distanceMatrixConfig,
+    params: {
+        ...distanceMatrixConfig.params,
+        origins: origins.join('|'),
+        destinations: destinations.join('|')
+    }
+});
 
 //Use the API call everywhere (Make it accessible throughout the whole application)
-export default{
+export default {
     NewNearByPlace,
+    calculateDistance,
     API_KEY
 }
