3a7a646bf504ffb49ca4c52450822259e7c21b04
Samantha Tan <samanthatan@Tans-MacBook-Air.local>
2024-03-25 13:35:05 +0800

    Favourite and unfavourite places complete

--- App/Screen/HomeScreen/PlaceListView.jsx
+++ App/Screen/HomeScreen/PlaceListView.jsx
@@ -8,24 +8,54 @@ import { Dimensions } from 'react-native';
 import { useEffect } from 'react';
 import { SelectMarkerContext } from '../../Context/SelectMarkerContext';
 import { useContext } from 'react';
+import { getFirestore, app } from '@firebase/firestore';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { FIREBASE_AUTH } from '../../../src/firebase/config';
+
 
 export default function PlaceListView({ placeList }) {
 
+    const user = FIREBASE_AUTH.currentUser
+    const [favList,setFavList] = useState([]);
+
     const flatListRef = useRef(null);
-    const {selectedMarker,setSelectedMarker}=useContext(SelectMarkerContext);
+    const { selectedMarker, setSelectedMarker } = useContext(SelectMarkerContext);
 
     useEffect(() => {
-        selectedMarker&&scrollToIndex(selectedMarker);
+        selectedMarker && scrollToIndex(selectedMarker);
     }, [selectedMarker]);
 
-    const scrollToIndex=(index)=>{
-        flatListRef.current?.scrollToIndex({animated:true,index})
+    const scrollToIndex = (index) => {
+        flatListRef.current?.scrollToIndex({ animated: true, index })
     }
-    const getItemLayout=(_,index)=>({
-        length:Dimensions.get('window').width,
-        offset:Dimensions.get('window').width*index,
+    const getItemLayout = (_, index) => ({
+        length: Dimensions.get('window').width,
+        offset: Dimensions.get('window').width * index,
         index
     });
+
+    const db = getFirestore(app);
+    useEffect(()=>{
+        user&&getFav();
+    },[user])
+
+    const getFav = async() => {
+        setFavList([])
+        const q = query(collection(db, "ev-fav-place"), where("email", "==", user?.email));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            setFavList(favList=>[...favList,doc.data()]);
+        });
+    }
+
+    const isFav=(place)=>{
+        const result=favList.find(item=>item.place.id==place.id);
+        console.log(result)
+        return result?true:false;
+    }
 
     const [selectedPlace, setSelectedPlace] = useState(null);
     const [expanded, setExpanded] = useState(false);
@@ -49,11 +79,14 @@ export default function PlaceListView({ placeList }) {
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item, index }) => (
                     <View key={index}>
-                        <PlaceItem place={item} onPress={handlePlacePress} />
+                        <PlaceItem place={item}
+                         isFav={isFav(item)}
+                         markedFav={()=>getFav()}
+                         onPress={handlePlacePress} />
                     </View>
                 )}
             />)}
-            {expanded && <View><ExpandedPlaceItem place={selectedPlace} onPress={handlePlacePress}/></View>}
+            {expanded && <View><ExpandedPlaceItem place={selectedPlace} onPress={handlePlacePress} /></View>}
         </View>
     );
 }
